885569e69f89746c781ba91cdc5cabce078f5e47
Andrew <106585100+Entrils@users.noreply.github.com>
2024-07-28 00:18:35 +0300

    first classes, provider, reducer

---
+++ src/Models/Match.js
@@ -0,0 +1,27 @@
+import {uid} from 'uid'
+
+class Match {
+
+    static get TEAM_ONE(){
+        return "TEAM_ONE";
+    }
+
+    static get TEAM_TWO(){
+        return "TEAM_TWO";
+    }
+
+    constructor(team_one,team_two){
+        this.id =uid()
+        this.team_one = team_one;
+        this.team_two = team_two;
+        this.winner = null;
+        this.score_one =0;
+        this.score_two = 0;
+    }
+
+
+    setWinner(winner){
+        this.winner = winner;
+    }
+}
+export default Match;
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux'
+import { rootReducer  } from './reducers';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(
+  loggerMiddleware
+))
+
+store.dispatch({type: "MEH"})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 reportWebVitals();
